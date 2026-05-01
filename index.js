f6ca3ef6b14251c465f8bb7f82542acaf0668c7d
dorianvelazquez <dorian.velazquez@students.msswarriors.org>
2026-05-01 18:58:09 +0000

    good

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let counter = 100;
+let prefixIncrement = ++counter;
+let postfixIncrement = counter++;
+let prefixDecrement = --counter;
+let postfixDecrement = counter--;
 
+document.getElementById("result").innerHTML = counter;
